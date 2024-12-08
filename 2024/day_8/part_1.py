f1c3cafe65fa79b40add7eb219ada4f6963b6d3d
Clément Grimaud <clement.grimaud@apiday.com>
2024-12-08 15:18:33 +0100

    Add day 8

---
+++ 2024/day_8/part_1.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+with open("input.txt", "r") as file:
+    data = file.read().splitlines()
+
+map_size = (len(data[0]), len(data))
+antinode_map = [[0] * map_size[0] for _ in range(map_size[1])]
+
+node_dict = defaultdict(list)
+
+# Create a dictionary of nodes with their coordinates
+for y in range(map_size[1]):
+    for x in range(map_size[0]):
+        if data[y][x] != ".":
+            node_dict[data[y][x]].append((x, y))
+
+for node in node_dict:
+    for antenna in node_dict[node]:
+        for second_antenna in node_dict[node]:
+            if antenna != second_antenna:
+                vector = (antenna[0] - second_antenna[0], antenna[1] - second_antenna[1])
+                antinode_coordinates = (antenna[0] + vector[0], antenna[1] + vector[1])
+                if 0 <= antinode_coordinates[0] < map_size[0] and 0 <= antinode_coordinates[1] < map_size[1]:
+                    antinode_map[antinode_coordinates[1]][antinode_coordinates[0]] = 1
+
+number_of_unique_location_with_antinode = sum([sum(row) for row in antinode_map])
+print(number_of_unique_location_with_antinode)

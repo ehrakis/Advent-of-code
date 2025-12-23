73fb686786d7c1b4e8f34ab7a18334067e0e5c25
Clément Grimaud <clement.grimaud@michelin.com>
2025-12-23 14:30:28 +0000

    Add 2025 day 11

---
+++ 2025/day_11/part_1.py
@@ -0,0 +1,41 @@
+with open("2025/day_11/input.txt", "r") as file:
+    data = file.read().splitlines()
+
+
+class Node:
+    def __init__(self, name, parent_node=None):
+        self.name = name
+        self.parent_node = [parent_node]
+        self.child_nodes = []
+        self.different_paths_to_output = 0
+
+    def _compute_number_of_paths_to_out(self):
+        return sum(
+            [
+                child.get_different_paths_to_output() if child.name != "out" else 1
+                for child in self.child_nodes
+            ]
+        )
+
+    def get_different_paths_to_output(self):
+        if not self.different_paths_to_output:
+            self.different_paths_to_output = self._compute_number_of_paths_to_out()
+
+        return self.different_paths_to_output
+
+
+nodes: dict[str, Node] = {}
+
+for line in data:
+    node_name, childs = line.split(":")
+    childs = childs.strip().split()
+
+    if not node_name in nodes:
+        nodes[node_name] = Node(node_name)
+
+    for child_node in childs:
+        if not child_node in nodes:
+            nodes[child_node] = Node(child_node, parent_node=nodes[node_name])
+        nodes[node_name].child_nodes.append(nodes[child_node])
+
+print(nodes["you"].get_different_paths_to_output())
